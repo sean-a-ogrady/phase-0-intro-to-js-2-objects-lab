6b2ae4de05a4e7757b71b5f7d0e740fb3896952e
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-08-24 21:20:04 -0400

    Implement functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {
+    name: "Gale Boetticher",
+    streetAddress: "6353 Juan Tabo Blvd, Albuquerque, New Mexico"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]: value};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const temp = {...employee};
+    delete temp[key];
+    return temp;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
